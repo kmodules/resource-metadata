01ba174d3aadc6a83a7ddcc9787e7afa29cff34f
Tamal Saha <tamal@appscode.com>
2020-05-06 00:46:30 -0700

    Fix tests
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- hack/test.sh
+++ hack/test.sh
@@ -23,5 +23,5 @@ export GOFLAGS="-mod=vendor"
 TARGETS=$(for d in "$@"; do echo ./$d/...; done)
 
 echo "Running tests:"
-go test -race -installsuffix "static" ${TARGETS}
+go test -short -race -installsuffix "static" ${TARGETS}
 echo
