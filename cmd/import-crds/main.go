4b538ce7c138b95059d74cd76355b217f4f8c274
Tamal Saha <tamal@appscode.com>
2021-02-12 00:18:18 -0800

    Import cert-manager crds (#88)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- cmd/import-crds/main.go
+++ cmd/import-crds/main.go
@@ -44,7 +44,7 @@ go run cmd/import-crds/main.go --input=/home/tamal/go/src/k8s.io/api/crds
 go run cmd/import-crds/main.go --input=/home/tamal/go/src/k8s.io/kube-aggregator/crds
 
 go run cmd/import-crds/main.go --input=/home/tamal/go/src/github.com/coreos/prometheus-operator/example/prometheus-operator-crd
-go run cmd/import-crds/main.go --input=/home/tamal/go/src/github.com/jetstack/cert-manager/deploy/charts/cert-manager/crds
+go run cmd/import-crds/main.go --input=/home/tamal/go/src/github.com/jetstack/cert-manager/deploy/crds
 go run cmd/import-crds/main.go --input=/home/tamal/go/src/github.com/appscode/voyager/api/crds
 go run cmd/import-crds/main.go --input=/home/tamal/go/src/stash.appscode.dev/apimachinery/crds
 go run cmd/import-crds/main.go --input=/home/tamal/go/src/kmodules.xyz/custom-resources/crds
