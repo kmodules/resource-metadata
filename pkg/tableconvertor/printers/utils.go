b26714b17470b9df6151318fe456cff9888a1892
Tamal Saha <tamal@appscode.com>
2022-03-01 19:31:24 -0800

    Fix linter warnings
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- pkg/tableconvertor/printers/utils.go
+++ pkg/tableconvertor/printers/utils.go
@@ -17,10 +17,8 @@ limitations under the License.
 package printers
 
 import (
-	"bytes"
 	"time"
 
-	core "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/duration"
 )
@@ -33,36 +31,4 @@ func translateTimestampSince(timestamp metav1.Time) string {
 	}
 
 	return duration.HumanDuration(time.Since(timestamp.Time))
-}
-
-// Lay out all the containers on one line if use wide output.
-func layoutContainerCells(containers []core.Container) (names string, images string) {
-	var namesBuffer bytes.Buffer
-	var imagesBuffer bytes.Buffer
-
-	for i, container := range containers {
-		namesBuffer.WriteString(container.Name)
-		imagesBuffer.WriteString(container.Image)
-		if i != len(containers)-1 {
-			namesBuffer.WriteString(",")
-			imagesBuffer.WriteString(",")
-		}
-	}
-	return namesBuffer.String(), imagesBuffer.String()
-}
-
-func printBoolPtr(value *bool) string {
-	if value != nil {
-		return printBool(*value)
-	}
-
-	return "<unset>"
-}
-
-func printBool(value bool) string {
-	if value {
-		return "True"
-	}
-
-	return "False"
 }
