d00553c6b47720ee409c96f79cecbd99a059ddad
Tamal Saha <tamal@appscode.com>
2022-03-07 02:46:00 -0800

    Format table definition files
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- cmd/resource-fmt/main.go
+++ cmd/resource-fmt/main.go
@@ -140,5 +140,5 @@ func main() {
 	MustCheckType(v1alpha1.ResourceDescriptor{}, "resourcedescriptors", *fix)
 	MustCheckType(v1alpha1.ResourceEditor{}, "resourceeditors", *fix)
 	MustCheckType(v1alpha1.ResourceOutline{}, "resourceoutlines", *fix)
-	// MustCheckType(v1alpha1.ResourceTableDefinition{}, "resourcetabledefinitions", *fix)
+	MustCheckType(v1alpha1.ResourceTableDefinition{}, "resourcetabledefinitions", *fix)
 }
