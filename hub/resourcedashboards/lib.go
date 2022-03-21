e90425370d621a2505071394d05a1bd5c04e55c7
Tamal Saha <tamal@appscode.com>
2022-03-20 23:34:55 -0700

    Add LoadByGVR for resource dashboard (#224)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- hub/resourcedashboards/lib.go
+++ hub/resourcedashboards/lib.go
@@ -17,6 +17,7 @@ limitations under the License.
 package resourcedashboards
 
 import (
+	"context"
 	"embed"
 	"fmt"
 	iofs "io/fs"
@@ -25,12 +26,15 @@ import (
 	"sort"
 	"sync"
 
+	kmapi "kmodules.xyz/client-go/api/v1"
 	"kmodules.xyz/resource-metadata/apis/meta/v1alpha1"
 
 	"github.com/pkg/errors"
 	ioutilx "gomodules.xyz/x/ioutil"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/klog/v2"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/yaml"
 )
 
@@ -73,7 +77,7 @@ var (
 
 				if obj.Spec.DefaultDashboards {
 					gvr := obj.Spec.Resource.GroupVersionResource()
-					expectedName := DefaultLayoutName(gvr)
+					expectedName := DefaultDashboardName(gvr)
 					if obj.Name != expectedName {
 						return fmt.Errorf("expected default %s name to be %s, found %s", reflect.TypeOf(v1alpha1.ResourceDashboard{}), expectedName, obj.Name)
 					}
@@ -106,7 +110,7 @@ func EmbeddedFS() iofs.FS {
 	return fs
 }
 
-func DefaultLayoutName(gvr schema.GroupVersionResource) string {
+func DefaultDashboardName(gvr schema.GroupVersionResource) string {
 	if gvr.Group == "" && gvr.Version == "v1" {
 		return fmt.Sprintf("core-v1-%s", gvr.Resource)
 	}
@@ -140,6 +144,33 @@ func LoadDefaultByGVR(gvr schema.GroupVersionResource) (*v1alpha1.ResourceDashbo
 
 	rv, found := rdPerGR[gvr]
 	return rv, found
+}
+
+func LoadByGVR(kc client.Client, gvr schema.GroupVersionResource) (*v1alpha1.ResourceDashboard, bool) {
+	var ed v1alpha1.ResourceDashboard
+	err := kc.Get(context.TODO(), client.ObjectKey{Name: DefaultDashboardName(gvr)}, &ed)
+	if err == nil {
+		return &ed, true
+	} else if client.IgnoreNotFound(err) != nil {
+		klog.V(3).InfoS(fmt.Sprintf("failed to load resource editor for %+v", gvr))
+	}
+	return LoadDefaultByGVR(gvr)
+}
+
+func LoadByResourceID(kc client.Client, rid *kmapi.ResourceID) (*v1alpha1.ResourceDashboard, bool) {
+	if rid == nil {
+		return nil, false
+	}
+
+	gvr := rid.GroupVersionResource()
+	if gvr.Version == "" || gvr.Resource == "" {
+		id, err := kmapi.ExtractResourceID(kc.RESTMapper(), *rid)
+		if client.IgnoreNotFound(err) != nil {
+			klog.V(3).InfoS(fmt.Sprintf("failed to extract resource id for %+v", *rid))
+		}
+		gvr = id.GroupVersionResource()
+	}
+	return LoadByGVR(kc, gvr)
 }
 
 func List() []v1alpha1.ResourceDashboard {
