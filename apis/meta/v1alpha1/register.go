ac68abcecde291645d5c034b581474f0eade9dc7
Tamal Saha <tamal@appscode.com>
2023-09-21 16:05:42 -0700

    Fix api (#365)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- apis/meta/v1alpha1/register.go
+++ apis/meta/v1alpha1/register.go
@@ -50,6 +50,8 @@ func Resource(resource string) schema.GroupResource {
 func addKnownTypes(scheme *runtime.Scheme) error {
 	scheme.AddKnownTypes(SchemeGroupVersion,
 		&ChartPresetQuery{},
+		&Project{},
+		&ProjectList{},
 		&ResourceDescriptor{},
 		&ResourceDescriptorList{},
 		&ResourceTableDefinition{},
