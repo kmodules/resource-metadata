e9df93bcdbd2487e95b90429e53ce9cbce71c913
Tamal Saha <tamal@appscode.com>
2024-05-09 09:27:59 +0600

    Add IsFeatureset{GR,GK} helpers
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- hub/helm.go
+++ hub/helm.go
@@ -25,6 +25,7 @@ import (
 	"kmodules.xyz/resource-metadata/apis/shared"
 
 	fluxsrc "github.com/fluxcd/source-controller/api/v1beta2"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	chartsapi "x-helm.dev/apimachinery/apis/charts/v1alpha1"
 	releasesapi "x-helm.dev/apimachinery/apis/releases/v1alpha1"
@@ -145,4 +146,12 @@ func HelmCreateNamespace(kc client.Client) bool {
 		return preset.Helm.CreateNamespace
 	}
 	return true
+}
+
+func IsFeaturesetGR(gr schema.GroupResource) bool {
+	return gr.Group == "ui.k8s.appscode.com" && gr.Resource == "featuresets"
+}
+
+func IsFeaturesetGK(gk schema.GroupKind) bool {
+	return gk.Group == "ui.k8s.appscode.com" && gk.Kind == "FeatureSet"
 }
