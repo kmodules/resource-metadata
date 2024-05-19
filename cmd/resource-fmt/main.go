40a13afa6d249c749ae789978ba158a56ac614b5
Tamal Saha <tamal@appscode.com>
2024-05-19 11:46:46 +0600

    Add ClusterProfile api
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- cmd/resource-fmt/main.go
+++ cmd/resource-fmt/main.go
@@ -145,6 +145,7 @@ func main() {
 	fix := flag.Bool("fix", true, "Fix formatting")
 	flag.Parse()
 
+	MustCheckType(metaapi.ClusterProfile{}, "clusterprofiles", *fix)
 	MustCheckType(metaapi.MenuOutline{}, "menuoutlines", *fix)
 	MustCheckType(metaapi.ResourceBlockDefinition{}, "resourceblockdefinitions", *fix)
 	MustCheckType(metaapi.ResourceDescriptor{}, "resourcedescriptors", *fix)
