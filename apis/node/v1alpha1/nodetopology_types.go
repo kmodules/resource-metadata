3a520e33c47a0b27b59e510c248322d8b54d6cc8
Tamal Saha <tamal@appscode.com>
2024-06-10 04:33:19 -0700

    Fix NodeTopology crd
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- apis/node/v1alpha1/nodetopology_types.go
+++ apis/node/v1alpha1/nodetopology_types.go
@@ -52,7 +52,6 @@ type NodeTopologySpec struct {
 	// Requirements are layered with GetLabels and applied to every node.
 	// +kubebuilder:validation:XValidation:message="requirements with operator 'In' must have a value defined",rule="self.all(x, x.operator == 'In' ? x.values.size() != 0 : true)"
 	// +kubebuilder:validation:XValidation:message="requirements operator 'Gt' or 'Lt' must have a single positive integer value",rule="self.all(x, (x.operator == 'Gt' || x.operator == 'Lt') ? (x.values.size() == 1 && int(x.values[0]) >= 0) : true)"
-	// +kubebuilder:validation:XValidation:message="requirements with 'minValues' must have at least that many values specified in the 'values' field",rule="self.all(x, (x.operator == 'In' && has(x.minValues)) ? x.values.size() >= x.minValues : true)"
 	// +kubebuilder:validation:MaxItems:=30
 	// +optional
 	Requirements []core.NodeSelectorRequirement `json:"requirements,omitempty" hash:"ignore"`
